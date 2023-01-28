34e6a648c14eb2cc732216e64c630ac2930e1fad
PetrenkoS <petrenko.s@yahoo.com>
2023-01-27 22:25:31 -0500

    Added login tests

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -21,6 +21,93 @@ public class LoginTests extends BaseTest {
         driver.quit();
     }
 
+    @Test
+    public static void LoginSucceedTest () {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String url = "https://bbb.testpro.io/";
+        driver.get(url);
+        WebElement emailInput = driver.findElement(By.cssSelector("[type='email']"));
+        emailInput.click();
+        emailInput.sendKeys("demo@class.com");
+        WebElement passwordInput = driver.findElement(By.cssSelector("[type='password']"));
+        passwordInput.click();
+        passwordInput.sendKeys("te$t$tudent");
+        WebElement submitLogin = driver.findElement(By.cssSelector("button[type='submit']"));
+        submitLogin.click();
+        WebElement avatar = driver.findElement(By.cssSelector(".avatar"));
+        Assert.assertTrue(avatar.isDisplayed());
+        driver.quit();
+    }
+
+
+    @Test
+    public static void LoginWrongPasswordTest () {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String url = "https://bbb.testpro.io/";
+        driver.get(url);
+        WebElement emailInput = driver.findElement(By.cssSelector("[type='email']"));
+        emailInput.click();
+        emailInput.sendKeys("demo@class.com");
+        WebElement passwordInput = driver.findElement(By.cssSelector("[type='password']"));
+        passwordInput.click();
+        passwordInput.sendKeys("student");
+        WebElement submitLogin = driver.findElement(By.cssSelector("button[type='submit']"));
+        submitLogin.click();
+        Assert.assertEquals(driver.getCurrentUrl(), url);
+        Assert.assertTrue(submitLogin.isDisplayed());
+        driver.quit();
+    }
+
+
+    @Test
+    public static void LoginEmptyPasswordTest () {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String url = "https://bbb.testpro.io/";
+        driver.get(url);
+        WebElement emailInput = driver.findElement(By.cssSelector("[type='email']"));
+        emailInput.click();
+        emailInput.sendKeys("demo@class.com");
+        WebElement submitLogin = driver.findElement(By.cssSelector("button[type='submit']"));
+        submitLogin.click();
+        Assert.assertEquals(driver.getCurrentUrl(), url);
+        Assert.assertTrue(submitLogin.isDisplayed());
+        WebElement registationLink = driver.findElement(By.id("hel"));
+        Assert.assertTrue(registationLink.isDisplayed(), "==== Registation link displayed ====");
+        driver.quit();
+    }
+
+    @Test
+    public static void LoginWrongEmailTest () {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String url = "https://bbb.testpro.io/";
+        driver.get(url);
+        WebElement emailInput = driver.findElement(By.cssSelector("[type='email']"));
+        emailInput.click();
+        emailInput.sendKeys("dem@class.com");
+        WebElement passwordInput = driver.findElement(By.cssSelector("[type='password']"));
+        passwordInput.click();
+        passwordInput.sendKeys("te$t$tudent");
+        WebElement submitLogin = driver.findElement(By.cssSelector("button[type='submit']"));
+        submitLogin.click();
+        Assert.assertEquals(driver.getCurrentUrl(), url);
+        Assert.assertTrue(submitLogin.isDisplayed());
+        WebElement registationLink = driver.findElement(By.id("hel"));
+        String link = registationLink.getText();
+        System.out.println("==== This is our link text ==== " + link);
+        driver.quit();
+    }
 
 
 }
